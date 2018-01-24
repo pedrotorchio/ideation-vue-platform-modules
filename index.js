74b4e9489b5545b1e5acd5a21e533f862b0371b9
pedrotorchio <pedrotorchio@gmail.com>
2018-01-24 14:27:13 -0300

    exportando page.vue

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ export {default as Base} from './Base';
 export {default as Element} from './Element';
 export {default as Installer} from './Installer';
 export {default as Module} from './Module';
+export {default as Page} from './Page.vue';
