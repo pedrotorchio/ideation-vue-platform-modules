e771ee4ae4e0c37d29dad2aaf1b51b7d546af091
pedrotorchio <pedrotorchio@gmail.com>
2018-01-24 14:49:05 -0300

    renomeando pagecomponent

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ export {default as Base} from './Base';
 export {default as Element} from './Element';
 export {default as Installer} from './Installer';
 export {default as Module} from './Module';
-export {default as Page} from './Page.vue';
+export {default as PageComponent} from './Page.vue';
